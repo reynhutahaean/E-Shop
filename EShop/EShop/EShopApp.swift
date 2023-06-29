a4e963c4e48f38a7c80876c7c3752db0e5e00f67
Reynaldo Hutahaean <reynhthaean@gmail.com>
2023-06-29 11:02:59 +0700

    initial commit

---
+++ EShop/EShop/EShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EShopApp.swift
+//  EShop
+//
+//  Created by Reynaldo Cristinus Hutahaean on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
